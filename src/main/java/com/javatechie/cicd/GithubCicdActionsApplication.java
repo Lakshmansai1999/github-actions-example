86a5e069a7fcbf932277e63c087c9eaf6b3da766
Lakshmansai1999 <94889893+Lakshmansai1999@users.noreply.github.com>
2023-12-20 09:51:24 +0530

    Update GithubCicdActionsApplication.java

--- src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/javatechie/cicd/GithubCicdActionsApplication.java
@@ -11,7 +11,7 @@ public class GithubCicdActionsApplication {
 
 	@GetMapping("/welcome")
 	public String welcome(){
-		return "Welcome to javatechie !";
+		return "This is a demo source!";
 	}
 
 //	echo "# github-actions-example" >> README.md
